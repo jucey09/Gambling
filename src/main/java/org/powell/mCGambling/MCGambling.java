6fe5417678ceb7ddac2157ac0324b54f26212032
Caleb Powell <powellcaleb09@gmail.com>
2025-04-15 14:43:38 -0400

    making gui

--- src/main/java/org/powell/mCGambling/MCGambling.java
+++ src/main/java/org/powell/mCGambling/MCGambling.java
@@ -1,17 +1,20 @@
 package org.powell.mCGambling;
 
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.powell.guiApi.GuiApi;
 
 public final class MCGambling extends JavaPlugin {
+    GuiApi guiApi;
 
     @Override
     public void onEnable() {
         // Plugin startup logic
+        guiApi = (GuiApi) getServer().getPluginManager().getPlugin("GuiApi");
 
-    }
+        getServer().getConsoleSender().sendMessage(ChatColor.AQUA + "MCGambling Enabled!");
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+        getCommand("mcg").setExecutor(new GambleCommand(this, guiApi));
     }
+    public GuiApi getGuiApi() { return guiApi; }
 }
